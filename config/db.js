809604ea9d9671f7ce93e94ef4ac1177f41e914e
Kush-Hash <ankush.ghosh.work@gmail.com>
2025-08-19 23:11:50 +0530

    update cs cert.

--- config/db.js
+++ config/db.js
@@ -3,7 +3,28 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 
-const pool = mysql.createPool(process.env.DB_URL);
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+import fs from "fs";
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: {
+        ca: fs.readFileSync(process.env.DB_CA)
+    }
+});
+
+export async function query(sql, params) {
+    const [rows] = await pool.execute(sql, params);
+    return rows;
+}
+
 
 export async function query(sql, params) {
     const [rows] = await pool.execute(sql, params);
